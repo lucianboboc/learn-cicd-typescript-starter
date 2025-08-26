8333020719f7de49ee8a7be6d0bc03cc99e33427
Lucian Boboc <info@lucianboboc.com>
2025-08-26 21:56:46 +0200

    Added auth test

---
+++ src/api/auth.test.ts
@@ -0,0 +1,14 @@
+import {describe, expect, test} from 'vitest';
+import { IncomingHttpHeaders } from "http";
+import {getAPIKey} from "./auth";
+
+const headers = {
+	"authorization": "ApiKey 123456",
+} as IncomingHttpHeaders;
+
+describe("auth", () => {
+	test("should return ApiKey", () => {
+		const apiKey = getAPIKey(headers);
+		expect(apiKey === "123456");
+	})
+});
